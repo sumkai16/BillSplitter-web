33c6eb7608a4b5e67149115a5370a16f67548329
Ricamaepatenio15 <rcmpatenio@gmail.com>
2026-02-28 03:51:44 +0800

    login, dashboard, and register + header modification

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -74,18 +74,22 @@ export default function Register() {
         transition={{ duration: 0.4 }}
         className="bg-white/80 backdrop-blur-md rounded-3xl shadow-2xl border border-white/40 w-full max-w-2xl p-10"
       >
-        <div className="text-center mb-10">
-          <div className="w-36 h-14 bg-gradient-to-r from-emerald-600 to-teal-600 rounded-2xl shadow-md flex items-center justify-center mx-auto mb-4">
-            <span className="text-2xl font-black text-white tracking-wide">
-              SPLITIFY
-            </span>
-          </div>
+        <div className="flex justify-center mb-6">
+        <img
+          src="public/hlogo.png"
+          alt="Logo"
+          className="w-48 h-auto object-contain"
+        />
+      </div>
 
-          <h1 className="text-3xl font-bold text-slate-800">Create Account</h1>
-          <p className="text-slate-500 mt-2 text-sm">
-            Join BillSplitter and split bills with ease
-          </p>
-        </div>
+      <div className="text-center mb-8">
+        <h1 className="text-2xl font-bold text-slate-800">
+          CREATE ACCOUNT
+        </h1>
+        <p className="text-slate-500 mt-1 text-sm">
+          Join BillSplitter and split bills with ease
+        </p>
+      </div>
 
         <form onSubmit={handleSubmit} className="space-y-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
